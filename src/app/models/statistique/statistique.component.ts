522c1e91b10a85ec5dfd881b9b57fd4981e3e385
Hugo <hugosoubey@gmail.com>
2021-06-29 12:43:10 +0200

    Exercice  4

--- src/app/models/statistique/statistique.component.ts
+++ src/app/models/statistique/statistique.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Statistique } from '../statistique';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-statistique',
@@ -10,6 +11,12 @@ export class StatistiqueComponent implements OnInit {
 
   lion = new Statistique("Lion", 50, 12000);
   tigre = new Statistique("Tigre", 65, 70000);
+  tabAnimal = [new Statistique("Panda", 10, 4500), new Statistique("Koala", 5, 60250)];
+
+  // Permet l'affichage de la ligne après 3 secondes
+  longueur = setInterval(() => {
+    this.tabAnimal.push(new Statistique("Rat", 14, 4500000));
+  }, 3000);
 
   constructor() { }
 
